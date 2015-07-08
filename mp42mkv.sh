bb6974548be32612c3c406fdfa917ea314b62dd7
John A Ginis <drxspace@gmail.com>
2015-07-08 19:51:48 +0300

    mkv

--- mp42mkv.sh
+++ mp42mkv.sh
@@ -19,10 +19,10 @@ INPUTPARAMS=""
 for f in *.mp4; do
 	[[ -f "${f}" ]] && {
 		[[ -f "${f%.*}".en.srt ]] && { LANGSPARAMS="--language\n0:eng\n--track-name\n0:English\n--default-track\n0:yes\n--forced-track\n0:no\n-s\n0\n-D\n-A\n-T\n--no-global-tags\n--no-chapters\n"$(printf "%s" "${f%.*}.en.srt\n"); }
-		[[ -f "${f%.*}".${ALTERLANG[0]}.srt ]] && { LANGSPARAMS=${LANGSPARAMS}"--language\n0:${ALTERLANG[1]}\n--track-name\n0:Ελληνικά\n--default-track\n0:no\n--forced-track\n0:no\n-s\n0\n-D\n-A\n-T\n--no-global-tags\n--no-chapters\n"$(printf "%s" "${f%.*}.${ALTERLANG[0]}.srt\n"); }
+		[[ -f "${f%.*}".${ALTERLANG[0]}.srt ]] && { LANGSPARAMS=${LANGSPARAMS}"--language\n0:${ALTERLANG[1]}\n--track-name\n0:Ελληνικά\n--forced-track\n0:no\n-s\n0\n-D\n-A\n-T\n--no-global-tags\n--no-chapters\n"$(printf "%s" "${f%.*}.${ALTERLANG[0]}.srt\n"); }
 
 		OUTPUTPARAMS="--output\n${f%.*}.mkv\n"
-		INPUTPARAMS="--forced-track\n0:no\n-a\n1\n-d\n0\n-S\n-T\n--no-global-tags\n--no-chapters\n${f}\n"
+		INPUTPARAMS="--forced-track\n0:no\n--forced-track\n1:no\n-a\n0\n-d\n1\n-S\n-T\n--no-global-tags\n--no-chapters\n${f}\n"
 
 		echo -e "${OUTPUTPARAMS}${GLOBALOPT}${LANGSPARAMS}${INPUTPARAMS}" > /tmp/mkvoptionsfile
 		sed -i -e 's/\\/\\\\/g' -e 's/ /\\s/g' -e 's/\"/\\2/g' -e 's/\:/\\c/g' -e 's/\#/\\h/g' /tmp/mkvoptionsfile
