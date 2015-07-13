f70a82ba15102d32e0acc4a6da38fb85d2a56343
John A Ginis <drxspace@gmail.com>
2015-07-13 17:54:42 +0300

    ycc

--- Arch/ycc.sh
+++ Arch/ycc.sh
@@ -15,8 +15,9 @@ fi
 if [[ -d /var/lib/pacman/sync ]]; then
 	if [[ -n $(pacman -Qqdt) ]]; then pacman -Rs $(pacman -Qqdt); fi
 	pacman -Scc
-	# Removes all the sync repositories .db also?
-	echo -en "\n:: \033[1mDo you want to remove all the sync repositories .db also? [y/N]: \033[0m"
+
+	echo -e "\nPacman sync repositories directory: /var/lib/pacman/sync"
+	echo -en ":: \033[1mDo you want to remove all the sync repositories databases? [y/N]: \033[0m"
 	read ANS
 	[[ ${ANS:-N} == [Yy] ]] && {
 		echo "removing all sync repositories..."
