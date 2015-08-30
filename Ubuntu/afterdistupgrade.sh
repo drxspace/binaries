fdde7ed055634e9413895a29d16bf0436e600281
John A Ginis <drxspace@gmail.com>
2015-08-30 10:00:14 +0300

    working version

--- Ubuntu/afterdistupgrade.sh
+++ Ubuntu/afterdistupgrade.sh
@@ -12,38 +12,44 @@ if [[ $EUID -ne 0 ]]; then
 	exec $(which sudo) "$0"
 fi
 
-Verbose=false
+Verbose=true
 
 nRepos=0
 nNewRepos=0
 ReleaseCodename="$(lsb_release -cs)"
 
-PPAisUP() {
+PPAisOK() {
 	[[ $(wget -q --no-check-certificate -S --spider "$1" 2>&1 | grep -E '^\s*HTTP.*?200') ]] && return 1
 	return 0
 }
 
 makeURL() {
-	local url="$(cat "$1" | grep -E "^deb[[:space:]]" | cut -d\  -f2- | tr \  \/)"
-	echo -n "${url//$ReleaseCodename/dists\/$ReleaseCodename}"
+	local url="$(cat "$1" | grep -E "^deb[[:space:]]" | cut -d\  -f2-)";
+	if [[ $(echo -n "$url" | wc -w) -gt 2 ]]; then
+		url="$(echo -n $url | tr \  \/)"
+		echo -n "${url//$ReleaseCodename/dists\/$ReleaseCodename}";
+	else
+		echo -n "${url% *}";
+	fi
 }
 
 grep -lE "^# deb[[:space:]]" /etc/apt/sources.list.d/*.list > /tmp/distupg.lst
 nRepos=$(wc -l < /tmp/distupg.lst)
 
 if [[ $nRepos -gt 0 ]]; then
-	notify-send "Reenable Repositories" "\nThere are $nRepos repositories that I'll try to reenable.\nPlease wait for this process to complete..." -i face-wink;
-	echo -en "\e[1;34m::\e[0;34m There are $nRepos repositories that I'll try to reenable.\n   Please wait for this process to complete..."
+	notify-send "Re-enable Repositories" "\nThere are $nRepos repositories that I'll try to re-enable.\nPlease wait for this process to complete..." -i face-wink;
+	echo -en "\e[1;34m::\e[0;34m There are $nRepos repositories that I'll try to re-enable.\n   Please wait for this process to complete..."
 
 	cat /tmp/distupg.lst | xargs -n1 sed -i -e 's/^# deb[[:space:]]/deb /' -e "s/ # disabled on upgrade to $(echo ${ReleaseCodename})//g"
 
 	for PPAfn in $(cat /tmp/distupg.lst); do
 		PPAurl=$(makeURL "$PPAfn");
-		$Verbose && echo -en "\e[0m\nURL in process: $PPAurl";
-		PPAisUP "$PPAurl";
+		$Verbose && echo -en "\e[0m\n-  URL in process: $PPAurl";
+		PPAisOK "$PPAurl";
 		return_val=$?;
 		if [[ $return_val -eq 1 ]]; then
 			: $(( nNewRepos++ ));
+			$Verbose && echo -en "\e[1;32m\n++ Re-enabled repository's URL: $PPAurl";
 		else
 			echo $PPAfn >> /tmp/distupg.new-lst;
 		fi;
@@ -51,17 +57,17 @@ if [[ $nRepos -gt 0 ]]; then
 
 	cat /tmp/distupg.new-lst | xargs -n1 sed -i -e 's/^deb[[:space:]]/# deb /' -e "/^# deb[[:space:]]/s/$/ # disabled on upgrade to $(echo ${ReleaseCodename})/"
 else
-	notify-send "Reenable Repositories" "\nThere are no repositories to reenable" -i face-smirk;
+	notify-send "Re-enable Repositories" "\nThere are no repositories to re-enable" -i face-smirk;
 fi
 
 rm -f /tmp/distupg.*
 
 if [[ $nNewRepos -gt 0 ]]; then
-	notify-send "Reenable Repositories" "\n$nNewRepos repositories were reenabled" -i face-wink;
-	echo -e "\e[0m\n\nDone. $nNewRepos repositories were reenabled."
+	notify-send "Re-enable Repositories" "\n$nNewRepos repositories were re-enabled" -i face-wink;
+	echo -e "\e[0m\n\nDone. $nNewRepos repositories were re-enabled."
 else
-	notify-send "Reenable Repositories" "\nNone repository was reenabled" -i face-worried;
-	echo -e "\e[0m\n\nDone. None repository was reenabled."
+	notify-send "Re-enable Repositories" "\nNone of the repositories was re-enabled" -i face-worried;
+	echo -e "\e[0m\n\nDone. None of the repositories was re-enabled."
 fi
 
 exit $?
