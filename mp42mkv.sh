cea8bb41c9a7c5db8335fcdd11b9d67ab9324b30
John A Ginis <drxspace@gmail.com>
2015-12-05 11:02:05 +0200

    wants track order

--- mp42mkv.sh
+++ mp42mkv.sh
@@ -7,7 +7,6 @@
 # \__,_/   /_/     /_/|_|  /_____/   _  .___/ /_/  |_|\____/   /_____/
 #                                    /_/           drxspace@gmail.com
 #
-
 #set -x
 
 Undot() {
@@ -15,8 +14,6 @@ Undot() {
 }
 
 rm -f /tmp/mkvoptionsfile
-
-GLOBALOPT="--disable-track-statistics-tags\n"
 
 declare -A ALTERLANG
 ALTERLANG[0,0]="el"
@@ -28,16 +25,22 @@ INPUTPARAMS=""
 IFS=$'\n\b'
 
 for f in $(ls *.{avi,mp4} 2>/dev/null); do
-	OUTPUTPARAMS=""
 	LANGSPARAMS=""
-	[[ -f "${f%.*}".en.srt ]] && { LANGSPARAMS="--language\n0:eng\n--track-name\n0:English subtitles\n--sub-charset\n0:UTF-8\n--default-track\n0:yes\n--forced-track\n0:no\n-s\n0\n-D\n-A\n-T\n--no-global-tags\n--no-chapters\n(\n""${f%.*}.en.srt""\n)\n"; }
-	[[ -f "${f%.*}".${ALTERLANG[0,0]}.srt ]] && { LANGSPARAMS=${LANGSPARAMS}"--language\n0:${ALTERLANG[0,1]}\n--track-name\n0:${ALTERLANG[0,2]}\n--sub-charset\n0:UTF-8\n--forced-track\n0:no\n-s\n0\n-D\n-A\n-T\n--no-global-tags\n--no-chapters\n(\n""${f%.*}.${ALTERLANG[0,0]}.srt""\n)\n"; }
+	
+	OUTPUTPARAMS="--ui-language\nen_US\n--output\n${f%.*}.mkv\n"
+	INPUTPARAMS="--language\n0:und\n--language\n1:und\n(\n${f}\n)\n--title\n$(Undot "${f%.*}")\n"
 
-	OUTPUTPARAMS="--output\n${f%.*}.mkv\n"
-	INPUTPARAMS="--forced-track\n0:no\n--forced-track\n1:no\n-a\n1\n-d\n0\n-S\n-T\n--no-global-tags\n--no-chapters\n--title\n$(Undot "${f%.*}")\n(\n${f}\n)\n"
+	[[ -f "${f%.*}".en.srt ]] && {
+		LANGSPARAMS="--sub-charset\n0:UTF-8\n--language\n0:eng\n--track-name\n0:English subtitles\n(\n""${f%.*}.en.srt""\n)\n";
+	}
+	[[ -f "${f%.*}".${ALTERLANG[0,0]}.srt ]] && {
+		LANGSPARAMS=${LANGSPARAMS}"--sub-charset\n0:UTF-8\n--language\n0:${ALTERLANG[0,1]}\n--track-name\n0:${ALTERLANG[0,2]}\n(\n""${f%.*}.${ALTERLANG[0,0]}.srt""\n)\n";
+	}
 
-	echo -e "${OUTPUTPARAMS}${GLOBALOPT}${LANGSPARAMS}${INPUTPARAMS}" > /tmp/mkvoptionsfile
-	sed -i -e 's/\\/\\\\/g' -e 's/ /\\s/g' -e 's/\"/\\2/g' -e 's/\:/\\c/g' -e 's/\#/\\h/g' /tmp/mkvoptionsfile
+	TRACKORDER="" #--track-order\n$(???)\n
+
+	echo -e "${OUTPUTPARAMS}${INPUTPARAMS}${LANGSPARAMS}${TRACKORDER}" | \
+	sed -e 's/\\/\\\\/g' -e 's/ /\\s/g' -e 's/\"/\\2/g' -e 's/\:/\\c/g' -e 's/\#/\\h/g' > /tmp/mkvoptionsfile
 
 	"$(which mkvmerge)" @/tmp/mkvoptionsfile || {
 		notify-send "MP4 to MKV convertor" "Problems with the converting proccess." -i face-worried;
