2f90753cffdfeba708358c4fbe6c85abb43af9d0
John A Ginis <drxspace@gmail.com>
2016-03-10 10:41:12 +0200

    transcript

---
+++ transcript.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# In this script, replace "tl=en" with the language into which you want the text
+# to be translated, for instance "tl=ru" for Russian, "tl=fr" for French and so on.
+
+#notify-send --icon=info "$(xsel -o)" "$(wget -U "Mozilla/5.0" -qO - "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=el&dt=t&q=$(xsel -o | sed "s/[\"'<>]//g")" | sed "s/,,,0]],,.*//g" | awk -F'"' '{print $2, $6}')"
+notify-send --icon=info "$(xsel -o)" "$(wget -U "Mozilla/5.0" -qO - "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=el&dt=t&q=$(xsel -o | sed "s/[\"'<>]//g")" | sed "s/,,,0]],,.*//g" | awk -F'"' '{print $2}')"
