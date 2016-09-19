44f7e3315f78708d1f9cf0a9daf1628385210ea0
John A Ginis <drxspace@gmail.com>
2016-09-19 21:01:00 +0300

    vtt

--- mp42mkv.sh
+++ mp42mkv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # _________        ____  ____________         _______ ___________________
 # ______  /__________  |/ /___  ____/________ ___    |__  ____/___  ____/
@@ -10,12 +10,12 @@
 #set -x
 
 Help() {
-  echo -e "mp42mkv: Showing help ... as always empty";
-  exit 20;
+	echo -e "mp42mkv: Showing help ... as always empty";
+	exit 20;
 }
 
 Undot() {
-  echo -n "${1//./ }";
+	echo -n "${1//./ }";
 }
 
 ConvTried=0
@@ -29,7 +29,7 @@ no=false
 INPUTPARAMS=""
 IFS=$'\n\b'
 
-NoLANGUAGES=2
+nLANGUAGES=2
 declare -A LANGUAGES
 # Enter your prefered language first to be set as the default
 LANGUAGES[0,0]="el"
@@ -42,41 +42,49 @@ LANGUAGES[1,1]="eng"
 LANGUAGES[1,2]="English subtitles"
 LANGUAGES[1,3]="ISO-8859-1"
 
+subExtension="srt"
+
 while [[ "$1" == -* ]]; do
-  case $1 in
-    -h)
-      # Show help
-      Help
-      ;;
-    -y)
-      yes=true
-      ;;
-    -n)
-      no=true
-      ;;
-     *)
-      WrongOption=$1
-      ;;
-  esac
-  shift
+	case $1 in
+	-h)
+		# Show help
+		Help
+		;;
+	-s)
+		subExtension="srt"
+		;;
+	-v)
+		subExtension="vtt"
+		;;
+	-y)
+		yes=true
+		;;
+	-n)
+		no=true
+		;;
+	 *)
+		WrongOption=$1
+		;;
+	esac
+	shift
 done
 
 # Check for option error
 if [[ "$WrongOption" != "" ]]; then
-  echo -e "mp42mkv: invalid option -- $WrongOption\nTry “mp42mkv -h” for more information.";
-  exit 10;
+	echo -e "mp42mkv: invalid option -- $WrongOption\nTry “mp42mkv -h” for more information.";
+	exit 10;
 fi
 if $yes && $no; then
-  echo -e "mp42mkv: invalid option coexistence\nTry “mp42mkv -h” for more information.";
-  exit 11;
+	echo -e "mp42mkv: invalid option coexistence\nTry “mp42mkv -h” for more information.";
+	exit 11;
 fi
 if [[ "$@" == "" ]]; then
-  FileArg="$(ls *.{avi,mp4} 2>/dev/null)";
+	FileArg="$(ls *.{avi,mp4} 2>/dev/null)";
 elif [[ -f "$@" ]]; then
-  FileArg="$@";
+	FileArg="$@";
 else
-  echo -e "mp42mkv: invalid filename “$@”";
-  exit 12;
+	echo -e "mp42mkv: invalid filename “$@”";
+	exit 12;
 fi
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -84,70 +92,70 @@ fi
 # main of script
 #
 for f in ${FileArg}; do
-  OUTPUTPARAMS="--ui-language\nen_US\n--output\n${f%.*}.mkv\n"
-  INPUTPARAMS="--language\n0:und\n--language\n1:und\n(\n${f}\n)\n"
+	OUTPUTPARAMS="--ui-language\nen_US\n--output\n${f%.*}.mkv\n"
+	INPUTPARAMS="--language\n0:und\n--language\n1:und\n(\n${f}\n)\n"
 
-  LANGSPARAMS=""
-  # We won't convert if no proper subtitle file(s) found for the current movie
-  WontConvert=true;
-  for ((k=0; k < NoLANGUAGES ; k++)); do
-    [[ -f "${f%.*}".${LANGUAGES[${k},0]}.srt ]] && {
-      # Check if we must convert subtitles file to utf-8 first
-      [[ -z $(file -bi "${f%.*}".${LANGUAGES[${k},0]}.srt | grep "utf-8" 2>/dev/null) ]] && {
-        echo -e "Converting subtitle file ${f%.*}.${LANGUAGES[${k},0]}.srt to “utf-8”";
-        mv "${f%.*}".${LANGUAGES[${k},0]}.srt "${f%.*}".${LANGUAGES[${k},0]}.srt.tmp;
-        iconv -f ${LANGUAGES[${k},3]} -t UTF8 -o "${f%.*}".${LANGUAGES[${k},0]}.srt "${f%.*}".${LANGUAGES[${k},0]}.srt.tmp && rm -f "${f%.*}".${LANGUAGES[${k},0]}.srt.tmp || {
-          mv "${f%.*}".${LANGUAGES[${k},0]}.srt.tmp "${f%.*}".${LANGUAGES[${k},0]}.srt;
-          echo -e "\e[0;30m\e[45mProblems with the conversion process of subtitle file ${f%.*}.${LANGUAGES[${k},0]}.srt to “utf-8”\e[0m";
-          continue;
-        }
-      }
-      WontConvert=false;
-      LANGSPARAMS=${LANGSPARAMS}"--sub-charset\n0:UTF-8\n--language\n0:${LANGUAGES[${k},1]}\n--track-name\n0:${LANGUAGES[${k},2]}\n(\n""${f%.*}.${LANGUAGES[${k},0]}.srt""\n)\n";
-    }
-  done
+	LANGSPARAMS=""
+	# We won't convert if no proper subtitle file(s) found for the current movie
+	WontConvert=true;
+	for ((k=0; k < nLANGUAGES ; k++)); do
+		[[ -f "${f%.*}".${LANGUAGES[${k},0]}.${subExtension} ]] && {
+			# Check if we must convert subtitles file to utf-8 first
+			[[ -z $(file -bi "${f%.*}".${LANGUAGES[${k},0]}.${subExtension} | grep "utf-8" 2>/dev/null) ]] && {
+				echo -e "Converting subtitle file ${f%.*}.${LANGUAGES[${k},0]}.${subExtension} to “utf-8”";
+				mv "${f%.*}".${LANGUAGES[${k},0]}.${subExtension} "${f%.*}".${LANGUAGES[${k},0]}.${subExtension}.tmp;
+				iconv -f ${LANGUAGES[${k},3]} -t UTF8 -o "${f%.*}".${LANGUAGES[${k},0]}.${subExtension} "${f%.*}".${LANGUAGES[${k},0]}.${subExtension}.tmp && rm -f "${f%.*}".${LANGUAGES[${k},0]}.${subExtension}.tmp || {
+					mv "${f%.*}".${LANGUAGES[${k},0]}.${subExtension}.tmp "${f%.*}".${LANGUAGES[${k},0]}.${subExtension};
+					echo -e "\e[0;30m\e[45mProblems with the conversion process of subtitle file ${f%.*}.${LANGUAGES[${k},0]}.${subExtension} to “utf-8”\e[0m";
+					continue;
+				}
+			}
+			WontConvert=false;
+			LANGSPARAMS=${LANGSPARAMS}"--sub-charset\n0:UTF-8\n--language\n0:${LANGUAGES[${k},1]}\n--track-name\n0:${LANGUAGES[${k},2]}\n(\n""${f%.*}.${LANGUAGES[${k},0]}.${subExtension}""\n)\n";
+		}
+	done
 
-  $WontConvert && {
-    echo -e "\e[1;33m\e[41mNone proper subtitle file(s) found for the movie “${f}”\e[0m";
-    continue;
-  }
+	$WontConvert && {
+		echo -e "\e[1;33m\e[41mSubtitle file(s) not found or not defined for the movie “${f}”\e[0m";
+		continue;
+	}
 
-  MOVIETITLE="--title\n$(Undot "${f%.*}")\n"
-  TRACKORDER="" #--track-order\n$(???)\n
+	MOVIETITLE="--title\n$(Undot "${f%.*}")\n"
+	TRACKORDER="" #--track-order\n$(???)\n
 
-  echo -e "${OUTPUTPARAMS}${INPUTPARAMS}${LANGSPARAMS}${MOVIETITLE}${TRACKORDER}" | \
-  sed -e 's/\\/\\\\/g' -e 's/ /\\s/g' -e 's/\"/\\2/g' -e 's/\:/\\c/g' -e 's/\#/\\h/g' > /tmp/mkvoptionsfile
+	echo -e "${OUTPUTPARAMS}${INPUTPARAMS}${LANGSPARAMS}${MOVIETITLE}${TRACKORDER}" | \
+	sed -e 's/\\/\\\\/g' -e 's/ /\\s/g' -e 's/\"/\\2/g' -e 's/\:/\\c/g' -e 's/\#/\\h/g' > /tmp/mkvoptionsfile
 
-  "$(which mkvmerge)" @/tmp/mkvoptionsfile ; RetCode=$?
-  : $(( ConvTried++ ));
+	"$(which mkvmerge)" @/tmp/mkvoptionsfile ; RetCode=$?
+	: $(( ConvTried++ ));
 
-  [[ $RetCode -gt 1 ]] && {
-    : $(( ConvError++ ));
-    echo -e "\e[1;33m\e[41mProblems with the conversion process of the movie “${f}”\e[0m";
-  } || {
-    [[ $RetCode -eq 1 ]] && {
-      : $(( ConvWarn++ ));
-      echo -e "\e[0;94mConversion process of the movie “${f}” done with warning(s)\e[0m";
-    } || {
-      : $(( ConvOkay++ ));
-      echo -e "\e[0;92mConversion process of the movie “${f}” done okay\e[0m";
-    }
-    { $yes || $no; } || read -p "Do you want to delete the converted files? [Y/n]: " ANS;
-    [[ ${ANS:-Y} == [Yy] ]] && { $no || rm -vf "${f%.*}"*.{avi,mp4,srt}; }
-  }
+	[[ $RetCode -gt 1 ]] && {
+		: $(( ConvError++ ));
+		echo -e "\e[1;33m\e[41mProblems with the conversion process of the movie “${f}”\e[0m";
+	} || {
+		[[ $RetCode -eq 1 ]] && {
+			: $(( ConvWarn++ ));
+			echo -e "\e[0;94mConversion process of the movie “${f}” done with warning(s)\e[0m";
+		} || {
+			: $(( ConvOkay++ ));
+			echo -e "\e[0;92mConversion process of the movie “${f}” done okay\e[0m";
+		}
+		{ $yes || $no; } || read -p "Do you want to delete the converted files? [Y/n]: " ANS;
+		[[ ${ANS:-Y} == [Yy] ]] && { $no || rm -vf "${f%.*}"*.{avi,mp4,${subExtension}}; }
+	}
 done
 
 if [[ $ConvTried -gt 0 ]]; then
-  rm -f /tmp/mkvoptionsfile
-  echo -e "--
+	rm -f /tmp/mkvoptionsfile
+	echo -e "--
 All conversion processes have finished.
 Tried $ConvTried movie(s).
 $(( ConvWarn + ConvOkay )) movie(s) converted to MKV.
 $ConvWarn movie(s) gave warning(s) and $ConvOkay done just fine.";
-  [[ $ConvError -gt 0 ]] && echo -e "$ConvError movie(s) aborted.";
+	[[ $ConvError -gt 0 ]] && echo -e "$ConvError movie(s) aborted.";
 else
-  echo -e "mp42mkv: No AVI nor MP4 file found to convert.";
-  exit 1;
+	echo -e "mp42mkv: No AVI nor MP4 file found to convert.";
+	exit 1;
 fi
 
 exit 0
