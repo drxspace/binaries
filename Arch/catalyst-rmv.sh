ce0ecd10309ef5ac4117c93c0ccafdae2ef4420e
John A Ginis <drxspace@gmail.com>
2016-10-09 20:06:35 +0300

    catalyst-rmv

--- Arch/catalyst-rmv.sh
+++ Arch/catalyst-rmv.sh
@@ -15,9 +15,11 @@ systemctl disable atieventsd
 systemctl disable temp-links-catalyst
 pacman -Rdd catalyst-total
 
-pacman -S xorg-apps xorg-fonts xf86-input-evdev xf86-video-ati
+pacman -S xorg-server xorg-server-common xorg-server-xwayland xorg-server-utils xorg-xinit mesa xorg-apps xorg-fonts xorg-twm xorg-xclock xterm
 
-pacman -S xorg-server xorg-server-utils xorg-xinit mesa xf86-video-vesa xorg-twm xorg-xclock xterm
+pacman -S xf86-input-evdev xf86-video-vesa xf86-video-ati
+
+cp -fv /etc/X11/xorg.conf.original-0 /etc/X11/xorg.conf
 
 pacman -S dkms linux linux-headers
 
