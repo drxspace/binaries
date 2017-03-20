8d6916a8ca5560214285171a0fceaabe6372cd0e
John A Ginis <drxspace@gmail.com>
2017-03-20 17:54:51 +0200

    country code

--- Arch/yup.sh
+++ Arch/yup.sh
@@ -7,8 +7,8 @@
 # \__,_/   /_/     /_/|_|  /_____/   _  .___/ /_/  |_|\____/   /_____/
 #                                    /_/           drxspace@gmail.com
 #
-#set -e
-#set -x
+#set -e set -x
+#set -x set -e
 
 ScriptName="$(basename $0)"
 
@@ -41,20 +41,19 @@ msg() {
 }
 
 ShowHelp() {
-	echo "${ScriptName} - Package manager helper utility" >&2
-	echo
-	echo "Usage: ${0##*/} [-c | --country] [-h | --help] [-m | --mirrors] [-o | --optimize] [-p | --purge] [-r | --refresh-keys]" >&2
-	echo
-	echo "Options:"
-	echo -e "  -c, --country\t\tTwo letters country code from where to generate the mirrorlist"
-	echo -e "\t\t\tUse the command \e[1mreflector --list-countries\e[0m to list them"
-	echo -e "\t\t\tThe \e[1m-m\e[0m option is implied"
-	echo -e "  -h, --help\t\tprint this help text and exit ;)"
-	echo -e "  -m, --mirrors\t\tretrieve and filter a list of the latest Arch Linux mirrors first"
-	echo -e "  -o, --optimize\tclean, upgrade and optimize pacman databases also"
-	echo -e "  -p, --purge\t\tclean ALL files from cache, unused and sync repositories databases also"
-	echo -e "  -r, --refresh-keys\trefresh pacman GnuPG keys"
-	exit 20;
+	echo -e "\e[1;38;5;209m${ScriptName}\e[0m - Package manager helper utility (depends on \e[1myaourt\e[0m -- https://archlinux.fr/yaourt-en and \e[1mreflector\e[0m -- https://wiki.archlinux.org/index.php/Reflector)" >&2
+	echo -e "\nUsage: ${0##*/} [-c | --country] [-h | --help] [-m | --mirrors] [-o | --optimize] [-p | --purge] [-r | --refresh-keys] [-u | --update]" >&2
+	echo -e "\nOptions:" >&2
+	echo -e "  -c, --country CODE\tTwo letters country code from where to generate the mirrorlist" >&2
+	echo -e "\t\t\tUse the command \e[1mreflector --list-countries\e[0m to list them" >&2
+	echo -e "\t\t\tThe \e[1m-m\e[0m option is implied" >&2
+	echo -e "  -h, --help\t\tPrint this help text and exit" >&2
+	echo -e "  -m, --mirrors\t\tRetrieve and filter a list of the latest Arch Linux mirrors" >&2
+	echo -e "  -o, --optimize\tClean, upgrade and optimize pacman databases" >&2
+	echo -e "  -p, --purge\t\tClean ALL files from cache, unused and sync repositories databases" >&2
+	echo -e "  -r, --refresh-keys\tRefresh pacman GnuPG keys" >&2
+	echo -e "  -u, --update\t\tUpgrades all packages that are out-of-date, package downgrades enabled" >&2
+	exit 10;
 }
 
 # reflector --list-countries
@@ -62,41 +61,52 @@ _CountryCodes=("AU" "AT" "BY" "BE" "BA" "BR" "BG" "CA" "CL" "CN" "CO" "HR" "CZ" 
 
 isCountry() {
 	local cc
-	for cc in "${_CountryCodes[@]}"; do [[ "$cc" == "$1" ]] && return 0; done
+	for cc in "${_CountryCodes[@]}"; do [[ "$cc" == $1 ]] && return 0; done
 	return 1
 }
-
 
 Mirrors=false
 Optimize=false
 Purge=false
 RefreshKeys=false
+Update=false
 nReflectorMirrors=10
 nReflectorMirrorsAge=12
 nReflectorThreads=4
-ReflectorCountry='DE' # DE (Denmark) is the default country code
+ReflectorCountry=''
 
 WrongOption=""
 
-
 yupRC="${HOME}"/.config/yuprc
-if [ -f "${yupRC}" ]; then
-	source "${yupRC}";
-else
+
+createRC() {
 	echo "# ${ScriptName} - Package manager helper utility config settings" > "${yupRC}";
 	echo "#" >> "${yupRC}";
 	echo "#" >> "${yupRC}";
 	echo "ReflectorCountry=${ReflectorCountry}" >> "${yupRC}";
+	return 1
+}
+
+if [ -f "${yupRC}" ]; then
+	source "${yupRC}";
+fi
+if [ -z "${ReflectorCountry}" ]; then
+	ReflectorCountry='DE' # DE (Denmark) is the default country code
+	createRC;
 fi
 
+
 while [[ "$1" == -* ]]; do
-	case $1 in
+	case "$1" in
 		-c | --country)
 			shift
-			isCountry "$1" && {
-				ReflectorCountry=$1;
+			isCountry "${1^^}" && {
+				ReflectorCountry=${1^^};
 				sed -i "/ReflectorCountry/s/=.*/=$(echo ${ReflectorCountry})/" "${yupRC}"
 				Mirrors=true;
+			} || {
+				msg "Invalid country code. Try “${ScriptName} -h” for more information" 1
+				exit 20;
 			}
 			;;
 
@@ -120,6 +130,10 @@ while [[ "$1" == -* ]]; do
 			RefreshKeys=true
 			;;
 
+		-u | --update)
+			Update=true
+			;;
+
 		 *)
 			WrongOption=$1
 			;;
@@ -128,23 +142,28 @@ while [[ "$1" == -* ]]; do
 done
 
 # Check options for error
-if [[ "$WrongOption" != "" ]] || [[ -n "$1" ]]; then
-	msg "Invalid option. Try “${ScriptName} -h” for more information" 2;
-	exit 10;
+if [[ "${WrongOption}" != "" ]] || [[ -n "$1" ]]; then
+	msg "Invalid option "${WrongOption}". Try “${ScriptName} -h” for more information" 1;
+	exit 30;
 fi
 
 if ! hash yaourt &>/dev/null; then
-	msg "\e[1myaourt\e[0m: command not found! See https://archlinux.fr/yaourt-en on how to install it" 2;
-	exit 1;
+	msg "\e[1myaourt\e[0m: command not found! See https://archlinux.fr/yaourt-en on how to install it" 1;
+	exit 40;
 fi
+
+
 
 # Grant root privileges
 sudo -v || exit 2
 
 if $RefreshKeys; then
+	# Grant root privileges for these too
+	sudo -v || exit 2
+
 	echo -e ":: \033[1mRefreshing pacman GnuPG keys...\033[0m"
 
-	sudo pacman -S --noconfirm gnupg archlinux-keyring antergos-keyring
+	sudo pacman -Sy --force --noconfirm gnupg archlinux-keyring antergos-keyring
 	sudo rm -rf /etc/pacman.d/gnupg
 	sudo pacman-key --init
 	sudo pacman-key --populate archlinux antergos
@@ -155,6 +174,9 @@ if $Mirrors; then
 	if ! hash reflector &>/dev/null; then
 		msg "\e[1mreflector\e[0m: command not found! Use \e[1msudo pacman -S reflector\e[0m to install it" 2;
 	else
+		# Grant root privileges for these too
+		sudo -v || exit 2
+
 		echo -e ":: \033[1mRetrieving and Filtering a list of the latest Arch Linux mirrors...\033[0m"
 
 		sudo $(which reflector) --country ${ReflectorCountry} --latest ${nReflectorMirrors} --age ${nReflectorMirrorsAge} --fastest ${nReflectorMirrors} --threads ${nReflectorThreads} --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
@@ -166,18 +188,37 @@ if $Mirrors; then
 	fi
 fi
 
+### Standard Action ###
+
 # -y, --refresh
 #	Passing two --refresh or -y flags will
 #	force a refresh of all package databases, even if they appear to be up-to-date.
-# -u, --sysupgrade
-#	Pass this option twice to enable package downgrades; in this case, pacman will select sync packages
-#	whose versions do not match with the local versions. This can be useful when the user switches from a
-#	testing repository to a stable one.
 # -a, --aur
 #	Also search in AUR database.
-yaourt --color -Syyuua
+yaourt --color -Syya
+
+### Standard Action ###
+
+#if $Update; then
+if $Update && [ $(yaourt -Qu | wc -l) -gt 0 ]; then
+	# Grant root privileges for these too
+	sudo -v || exit 2
+
+	echo -e "\n:: \033[1mUpdating packages...\033[0m"
+
+#	 -u, --sysupgrade
+#		Pass this option twice to enable package downgrades; in this case, pacman will select sync packages
+#		whose versions do not match with the local versions. This can be useful when the user switches from a
+#		testing repository to a stable one.
+#	-a, --aur
+#		With -u or --sysupgrade, upgrade aur packages that are out of date.
+	yaourt --color -Suua
+fi
 
 if $Optimize; then
+	# Grant root privileges for these too
+	sudo -v || exit 2
+
 	echo -e "\n:: \033[1mCleaning, Upgrading and Optimizing pacman databases...\033[0m"
 
 	sudo pacman --color always -Scc --noconfirm
@@ -186,6 +227,9 @@ if $Optimize; then
 fi
 
 if $Purge; then
+	# Grant root privileges for these too
+	sudo -v || exit 2
+
 	echo -e "\n:: \033[1mCleaning ALL files from cache, unused and sync repositories databases...\033[0m"
 
 	if [[ -d /var/lib/pacman/sync ]]; then
@@ -199,7 +243,8 @@ if $Purge; then
 		#	Use one --clean switch to only remove packages that are no
 		#	longer installed; use two to remove all files from the cache. In both cases, you will have a yes or no
 		#	option to remove packages and/or unused downloaded databases.
-		sudo pacman --color always -Scc
+		#sudo pacman --color always -Scc
+		yaourt -Scc
 
 		echo -e "\nPacman sync repositories directory: /var/lib/pacman/sync"
 		echo -en ":: \033[1mDo you want to remove ALL the sync repositories databases? [y/N] \033[0m"
